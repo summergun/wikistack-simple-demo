16453712f9998eb29d2a20a6aab5090781739908
Mauro Restuccia <mrestuccia@mac.com>
2017-02-12 15:19:40 -0500

    1st commit

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const server = require('http').createServer(require('./app'));
 
 const port = process.env.PORT || 3000;
 
-
 server.listen(port, ()=> console.log(`listening on ${port}`));
 
 db.seed()
